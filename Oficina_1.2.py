99f9706e9c0faf84ae9fa634846ab24bfd793e10
Yuri-S-Cardoso <yuridscardoso@gmail.com>
2026-07-26 20:03:59 -0300

    att

--- Oficina_1.2.py
+++ Oficina_1.2.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 df = pd.read_csv("car_data.csv") 
 #print(df.head()) # printa as primeiras linhas do dataframe
@@ -27,8 +30,6 @@ x = df[['ano', 'preco_atual', 'kms_rodados', 'tipo_combustivel', 'tipo_vendedor'
 
 y = df[['preco_venda']]
 
-from sklearn.model_selection import train_test_split
-
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 print('Treino')
@@ -38,8 +39,6 @@ print("y_train: ",y_train.shape)
 print('\nTeste')
 print("x_test: ",x_test.shape)
 print("y_test: ",y_test.shape)
-
-from sklearn.linear_model import LinearRegression
 
 # define um modelo de regressão
 model = LinearRegression()
@@ -59,8 +58,6 @@ plt.ylabel('Preço')
 plt.xlabel('Índice')
 
 plt.show()
-
-from sklearn.metrics import r2_score
 
 print('R2-score: ', r2_score(y_test, y_pred))
 
