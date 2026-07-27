61336718f84a2814a1a24ca2006dc771d912c32a
Yuri-S-Cardoso <yuridscardoso@gmail.com>
2026-07-27 18:34:57 -0300

    nova aula

--- Aula_2.3.py
+++ Aula_2.3.py
@@ -12,7 +12,22 @@ y = data['fruit_label']
 
 X_train, X_test, y_train, y_test = train_test_split(x,y)
 
-mm == MinMaxScaler()
+mm = MinMaxScaler()
 
-# esta liha 
+# estamos usando este comando para pegar os dados de train e transformando cada coluna 
+# em dados entre 0 e 1 e jogando na própria variável de conjuntode train
 X_train = mm.fit_transform(X_train)
+
+# Nessa linha, estamos utilizando apenas "mm.transform". Nesta  segunda linha, estamos 
+# apenas transformando os dados de acordo com a escala efetuada nos dados de treino, pois 
+# os dados de treino serão utilizados para preparar  e  construir  o  modelo.  Então,  
+# toda  transformação  deve  ser  baseada  na transformação executada nos dados de treino
+X_test = mm.transform(X_test)
+print(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=3)
+
+print(knn.fit(X_train,y_train))
+print(knn.score(X_test,y_test))
+print(knn.predict(X_test))
+print(y_test)
